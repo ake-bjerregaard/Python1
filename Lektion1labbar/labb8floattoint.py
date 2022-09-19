1a8706bdbc9899fb95b377e485eceade0e5b4adb
ake-bjerregaard <113858998+ake-bjerregaard@users.noreply.github.com>
2022-09-19 15:47:57 +0200

    Add files via upload
    Lektion 1 igen

---
+++ Lektion1labbar/labb8floattoint.py
@@ -0,0 +1,13 @@
+#converting av flyttal
+#flyttal tappar decimaldelen, även kallat floor
+heltal = int(5.0)
+
+print(heltal)
+
+heltal = int(5.1)
+
+print(heltal)
+
+heltal = int(5.999)
+
+print(heltal)

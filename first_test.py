ba4c3397bc012c07bbb75d1d2ae474d9c3d8eb0b
Åke aa <ake.bje@privat.se>
2022-09-19 07:46:07 +0200

    Also test english

--- first_test.py
+++ first_test.py
@@ -4,7 +4,14 @@ from test_methods import simple_write_with_sign
 
 signs = programming_signs()
 
+Sign.swedish = True
+
+for k, v in signs.items():
+    simple_write_with_sign(v)
+    
+Sign.swedish = False
+
 for k, v in signs.items():
     simple_write_with_sign(v)
 
-print_correct("Hello world!", "Write a print function with that writes \"Hello world!\" exactly!")
+# print_correct("Hello world!", "Write a print function with that writes \"Hello world!\" exactly!")
